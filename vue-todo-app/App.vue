87aaef95c89dc5785da0503256b9eaa62dd1c1b9
jsna <wlstnrsk23>
2020-12-21 00:25:01 +0900

    add 03.- 01-10

---
+++ vue-todo-app/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<h1>{{ msg }}</h1>
+</template>
+
+<script>
+	export default {
+		data () {
+			return {
+				msg: 'Helloooooo'
+				
+			}
+		}
+	}
+</script>
+
+<style scoped>
+h1{
+	color:red
+}
+</style>
