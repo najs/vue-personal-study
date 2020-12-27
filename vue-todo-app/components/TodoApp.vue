655c0009180c9fdcfee10b70f61b3dd9428dbe7e
jsna <wlstnrsk23>
2020-12-27 15:17:15 +0900

    study 04.04-08

--- vue-todo-app/components/TodoApp.vue
+++ vue-todo-app/components/TodoApp.vue
@@ -1,17 +1,56 @@
 <template>
 	<div>
 		<todo-item />
-		<todo-creator />
+		<todo-creator @create-todo="createTodo" />
 	</div>
 </template>
 
 <script>
+	import lowdb from 'lowdb'
+	import LocalStorage from 'lowdb/adapters/LocalStorage'
+	import cryptoRandomString from 'crypto-random-string'
 	import TodoCreator from './TodoCreator'
 	import TodoItem from './TodoItem'
 	export default {
 		components: {
 			TodoCreator,
 			TodoItem
+		},
+		data () {
+			return{
+				db: null
+			}
+		},
+		created () {
+			this.initDB()
+		},
+		methods: {
+			initDB () {
+				const adapter = new LocalStorage('todo-app'); // DB
+				this.db = lowdb(adapter);
+				
+				console.log(this.db);
+				
+				// Local DB init
+				this.db
+					.defaults({
+						todos: [] //Collection
+					}).write()
+			},
+			createTodo (title) {
+				const newTodo = {
+					id: cryptoRandomString({ length: 10 }),
+					title: title,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+					done: false
+				};
+				
+				this.db
+					.get('todos') // lodash
+					.push(newTodo) // lodash
+					.write() // lowdb
+			}
 		}
 	}
 </script>
--- vue-todo-app/components/TodoCreator.vue
+++ vue-todo-app/components/TodoCreator.vue
@@ -1,10 +1,37 @@
 <template>
-	<h1>Todo Creator</h1>
+	<div>
+		<button @click="createTodo">add</button>
+		<input
+			:value="title"
+			:placeholder="placeholder"
+			@input="title = $event.target.value"
+			type="text"
+			@keypress.enter="createTodo"
+		/>
+	</div>
 </template>
 
 <script>
 	export default {
-		name:"TodoCreator"
+		data (){
+			return {
+				title: '',
+				placeholder:"할 일을 추가해주시어요!"
+			}
+		},
+		methods: {
+			createTodo () {
+				const validatedTitle = this.title && this.title.trim();
+				
+				if (!validatedTitle) {
+					alert('유효하지 않은 제목입니다.');
+					this.title = this.title.trim();
+					return
+				}
+				this.$emit('create-todo', this.title);
+				this.title = ''
+			}
+		}
 	}
 </script>
 
