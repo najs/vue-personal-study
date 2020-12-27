5ac702f32069ebed92c161134679837c020c9435
jsna <wlstnrsk23>
2020-12-28 00:25:07 +0900

    study 04.09-13

--- vue-todo-app/components/TodoApp.vue
+++ vue-todo-app/components/TodoApp.vue
@@ -1,6 +1,13 @@
 <template>
 	<div>
-		<todo-item />
+		<todo-item
+			v-for="todo in todos"
+			:key="todo.id"
+			:todo="todo"
+			@update-todo="updateTodo"
+			@delete-todo="deleteTodo"
+		/>
+		<hr>
 		<todo-creator @create-todo="createTodo" />
 	</div>
 </template>
@@ -9,8 +16,14 @@
 	import lowdb from 'lowdb'
 	import LocalStorage from 'lowdb/adapters/LocalStorage'
 	import cryptoRandomString from 'crypto-random-string'
+	import _cloneDeep from 'lodash/cloneDeep'
+	import _find from 'lodash/find'
+	import _assign from 'lodash/assign'
+	import _findIndex from 'lodash/findIndex'
 	import TodoCreator from './TodoCreator'
 	import TodoItem from './TodoItem'
+	
+	
 	export default {
 		components: {
 			TodoCreator,
@@ -18,7 +31,8 @@
 		},
 		data () {
 			return{
-				db: null
+				db: null,
+				todos : []
 			}
 		},
 		created () {
@@ -31,16 +45,22 @@
 				
 				console.log(this.db);
 				
-				// Local DB init
-				this.db
-					.defaults({
-						todos: [] //Collection
-					}).write()
-			},
+				const hasTodos = this.db.has('todos').value() //lowdb
+				
+				if (hasTodos) {
+					this.todos = _cloneDeep(this.db.getState().todos)
+				} else {
+					// Local DB init
+						this.db
+							.defaults({
+								todos: [] //Collection
+							}).write()
+					}
+				},
 			createTodo (title) {
 				const newTodo = {
 					id: cryptoRandomString({ length: 10 }),
-					title: title,
+					title,
 					createdAt: new Date(),
 					updatedAt: new Date(),
 					done: false
@@ -50,6 +70,28 @@
 					.get('todos') // lodash
 					.push(newTodo) // lodash
 					.write() // lowdb
+				
+				// Create Client
+				this.todos.push(newTodo)
+			},
+			updateTodo(todo, value) {
+				this.db
+					.get('todos')
+					.find({ id: todo.id })
+					.assign(value)
+					.write();
+				
+				const foundTodo = _find(this.todos, {id: todo.id});
+				_assign(foundTodo, value)
+			},
+			deleteTodo(todo) {
+				this.db
+					.get('todos')
+					.remove({ id: todo.id })
+					.write();
+
+				const foundIndex = _findIndex(this.todos, { id: todo.id });
+				this.$delete(this.todos, foundIndex)
 			}
 		}
 	}
--- vue-todo-app/components/TodoItem.vue
+++ vue-todo-app/components/TodoItem.vue
@@ -1,10 +1,126 @@
 <template>
-	<h2>Todo Item</h2>
+	<div class="todo-item">
+		
+		<div
+			v-if="isEditMode"
+			class="item__inner item--edit">
+			<input
+				ref="titleInput"
+				:value="editedTItle"
+				type="text"
+				@input="editedTItle = $event.target.value"
+				@keypress.enter="editedTodo"
+				@keypress.esc="offEditMode"
+			/>
+			<div class="item__actions">
+				<button
+					key="complete"
+					@click="editedTodo">완료</button>
+				<button
+					key="cancel"
+					@click="offEditMode">취소</button>
+			</div>
+		</div>
+		
+		<div
+			v-else
+			class="item__inner item--normal"
+		>
+			<label>
+				<input
+					v-model="done"
+					type="checkbox"
+				/>
+			</label>
+			<div class="item__title-wrap">
+				<div class="item__title">
+					{{ todo.title }}
+				</div>
+				<div class="item__date">
+					{{ date }}
+				</div>
+			</div>
+			<div class="item__actions">
+				<button
+					key="update"
+					@click="onEditMode">수정</button>
+				<button
+					key="delete"
+					@click="deleteTodo">삭제</button>
+			</div>
+		
+		</div>
+		
+		
+	</div>
 </template>
 
 <script>
+	import dayjs from 'dayjs'
+	
+	
 	export default {
-		name:"TodoItem"
+		name:"TodoItem",
+		props:{
+			todo: Object
+		},
+		data (){
+			return {
+				isEditMode: false,
+				editedTItle: ''
+			}
+			
+		},
+		computed: {
+			done: {
+				get () {
+					return this.todo.done
+				},
+				set (done) {
+					this.updateTodo({
+						done
+					})
+				}
+			},
+			date () {
+				const date = dayjs(this.todo.createdAt);
+				const isSame = date.isSame(this.todo.updatedAt);
+				
+				if(isSame) {
+					return date.format('YYYY년 MM월 DD일')
+				} else {
+					return `${date.format('YYYY년 MM월 DD일')} (edited)`
+				}
+			}
+		},
+		methods: {
+			editedTodo(){
+				if (this.title !== this.editedTItle) {
+					this.updateTodo({
+						title: this.editedTItle,
+						updatedAt: new Date()
+					});
+					this.offEditMode()
+				}
+			},
+			onEditMode(){
+				this.isEditMode = true;
+				this.editedTItle = this.todo.title;
+				//Vue.js가 데이터 변경 후 DOM 업데이트를 마칠 때까지 기다림.
+				this.$nextTick(() => {
+					this.$refs.titleInput.focus()
+				})
+			},
+			offEditMode(){
+				this.isEditMode = false;
+			},
+			updateTodo(value){
+				this.$emit('update-todo', this.todo, value)
+			},
+			deleteTodo(){
+				this.$emit('delete-todo', this.todo)
+			}
+		}
 	}
 </script>
 
